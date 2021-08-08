57d5d42b7407e0a48400915172ff2747ffd556e2
Roman Papusha <shashilx@shashilx.xyz>
2021-08-08 18:42:44 +0300

    Update initialsetup.sh

--- initialsetup.sh
+++ initialsetup.sh
@@ -10,5 +10,5 @@ curl -fsSL http://www.tataranovich.com/debian/gpg | apt-key add -
 APT_SOURCE='deb http://www.tataranovich.com/debian buster main backports'
 echo $APT_SOURCE | tee /etc/apt/sources.list.d/tataranovich.list
 apt update
-apt install mc gpm arj dbview genisoimage links odt2txt zip
+apt -y install mc gpm arj dbview genisoimage links odt2txt zip
 apt -y full-upgrade
