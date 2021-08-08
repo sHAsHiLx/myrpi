762f979d60ced552a88705eb10c3eb914d428ad1
Roman Papusha <shashilx@shashilx.xyz>
2021-08-08 18:25:05 +0300

    Create initialsetup.sh

---
+++ initialsetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sed -i 's/# en_GB/en_GB/g' /etc/locale.gen
+sed -i 's/# en_US/en_US/g' /etc/locale.gen
+sed -i 's/# ru_RU/ru_RU/g' /etc/locale.gen
+sed -i 's/# ru_UA/ru_UA/g' /etc/locale.gen
+sed -i 's/# uk_UA/uk_UA/g' /etc/locale.gen
+locale-gen
+update-locale "LANG=ru_UA.UTF-8"
+curl -fsSL http://www.tataranovich.com/debian/gpg | apt-key add -
+APT_SOURCE='deb http://www.tataranovich.com/debian sid main'
+echo $APT_SOURCE | tee /etc/apt/sources.list.d/tataranovich.list
+apt update
+apt install mc gpm arj dbview genisoimage links odt2txt zip
+apt -y full-upgrade
