6fff8e5b9ad718843a52257b8e33d6f5fb62ba13
Roman Papusha <shashilx@shashilx.xyz>
2021-08-08 20:10:57 +0300

    Create hestiacp-latest-release.sh

---
+++ hestiacp-latest-release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+versions=($(git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' https://github.com/hestiacp/hestiacp|awk '{ print $2 }'|sed 's/refs\/tags\///g'))
+echo "Latest version of HestiaCP is ${versions[-1]}"
+echo "Cloning..."
+git clone -b ${versions[-1]} https://github.com/hestiacp/hestiacp hestiacp-${versions[-1]}
+cd hestiacp-${versions[-1]}/src
+./hst_autocompile.sh --all --noinstall --keepbuild '~localsrc'
