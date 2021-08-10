c36c1bdd74009251bba062b4f8f9fb674438b079
Roman Papusha <shashilx@shashilx.xyz>
2021-08-10 15:23:06 +0300

    Update initialsetup.sh

--- initialsetup.sh
+++ initialsetup.sh
@@ -26,7 +26,7 @@ dphys-swapfile setup
 dphys-swapfile swapon
 
 # Setting static IP
-cat << EOF | tee /etc/dhcpcd.conf
+cat << EOF >> /etc/dhcpcd.conf
 static ip_address=192.168.28.11/24
 static routers=192.168.28.1
 static domain_name_servers=192.168.28.5
